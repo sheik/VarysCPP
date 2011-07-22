78619ee1c7d7e4e6e9588b74207aaef20f61f983
Jeff Aigner <enumeration@gmail.com>
2011-07-21 22:48:41 -0500

    Refactored IRCClient out of the header file

--- IRCClient.cpp
+++ IRCClient.cpp
@@ -7,3 +7,89 @@
 
 #include "IRCClient.hpp"
 
+IRCClient::IRCClient(boost::asio::io_service& io_service,
+        tcp::resolver::iterator endpoint_iterator)
+: io_service_(io_service),
+socket_(io_service) {
+    tcp::endpoint endpoint = *endpoint_iterator;
+    socket_.async_connect(endpoint,
+            boost::bind(&IRCClient::handle_connect, this,
+            boost::asio::placeholders::error, ++endpoint_iterator));
+}
+
+void IRCClient::write(const std::string& msg) {
+    io_service_.post(boost::bind(&IRCClient::do_write, this, msg));
+}
+
+void IRCClient::close() {
+    io_service_.post(boost::bind(&IRCClient::do_close, this));
+}
+
+void IRCClient::handle_connect(const boost::system::error_code& error,
+        tcp::resolver::iterator endpoint_iterator) {
+    if (!error) {
+        do_write("USER test test test :test\r\n");
+        do_write("NICK test\r\n");
+        boost::asio::async_read_until(socket_, _response, "\r\n",
+                boost::bind(&IRCClient::handle_read, this,
+                boost::asio::placeholders::error));
+    } else if (endpoint_iterator != tcp::resolver::iterator()) {
+        socket_.close();
+        tcp::endpoint endpoint = *endpoint_iterator;
+        socket_.async_connect(endpoint,
+                boost::bind(&IRCClient::handle_connect, this,
+                boost::asio::placeholders::error, ++endpoint_iterator));
+    }
+}
+
+void IRCClient::handle_read(const boost::system::error_code& error) {
+    std::istream recv_stream(&_response);
+    std::string line;
+    if (!error) {
+        while (std::getline(recv_stream, line)) {
+            this->process_message(IRCMessage(line));
+        }
+        boost::asio::async_read_until(socket_, _response, "\r\n",
+                boost::bind(&IRCClient::handle_read, this,
+                boost::asio::placeholders::error));
+    } else {
+        do_close();
+    }
+}
+
+void IRCClient::do_write(std::string msg) {
+    bool write_in_progress = !write_messages_.empty();
+    this->write_messages_.push_back(msg);
+
+    if (!write_in_progress) {
+        boost::asio::async_write(socket_,
+                boost::asio::buffer(write_messages_.front(),
+                write_messages_.front().length()),
+                boost::bind(&IRCClient::handle_write, this,
+                boost::asio::placeholders::error));
+    }
+}
+
+void IRCClient::handle_write(const boost::system::error_code& error) {
+    if (!error) {
+        write_messages_.pop_front();
+        if (!write_messages_.empty()) {
+            boost::asio::async_write(socket_,
+                    boost::asio::buffer(write_messages_.front(),
+                    write_messages_.front().length()),
+                    boost::bind(&IRCClient::handle_write, this,
+                    boost::asio::placeholders::error));
+        }
+    } else {
+        do_close();
+    }
+}
+
+void IRCClient::do_close() {
+    socket_.close();
+}
+
+void IRCClient::process_message(IRCMessage msg) {
+    std::cout << msg.get_message() << std::endl;
+    std::cout << msg.get_command() << std::endl;
+}
--- IRCClient.hpp
+++ IRCClient.hpp
@@ -21,104 +21,27 @@ typedef std::deque<std::string> irc_message_queue;
 
 class IRCClient {
 public:
-
     IRCClient(boost::asio::io_service& io_service,
-            tcp::resolver::iterator endpoint_iterator)
-    : io_service_(io_service),
-    socket_(io_service) {
-        tcp::endpoint endpoint = *endpoint_iterator;
-        socket_.async_connect(endpoint,
-                boost::bind(&IRCClient::handle_connect, this,
-                boost::asio::placeholders::error, ++endpoint_iterator));
-    }
+            tcp::resolver::iterator endpoint_iterator);
+    void write(const std::string& msg);
+    void close();
     
-    void write(const std::string& msg) {
-        io_service_.post(boost::bind(&IRCClient::do_write, this, msg));
-    }
-
-    void close() {
-        io_service_.post(boost::bind(&IRCClient::do_close, this));
-    }
-
-private:
-
-    void handle_connect(const boost::system::error_code& error,
-            tcp::resolver::iterator endpoint_iterator) {
-        if (!error) {
-            do_write("USER test test test :test\r\n");
-            do_write("NICK test\r\n");
-            boost::asio::async_read_until(socket_, _response, "\r\n",
-                    boost::bind(&IRCClient::handle_read, this,
-                    boost::asio::placeholders::error));
-        } else if (endpoint_iterator != tcp::resolver::iterator()) {
-            socket_.close();
-            tcp::endpoint endpoint = *endpoint_iterator;
-            socket_.async_connect(endpoint,
-                    boost::bind(&IRCClient::handle_connect, this,
-                    boost::asio::placeholders::error, ++endpoint_iterator));
-        }
-    }
-
-    void handle_read(const boost::system::error_code& error) {
-        std::istream recv_stream(&_response);
-        std::string line;
-        if (!error) {
-            while (std::getline(recv_stream, line)) {
-                this->process_message(IRCMessage(line));
-            }
-            boost::asio::async_read_until(socket_, _response, "\r\n",
-                    boost::bind(&IRCClient::handle_read, this,
-                    boost::asio::placeholders::error));
-        } else {
-            do_close();
-        }
-    }
-
-    void do_write(std::string msg) {
-        bool write_in_progress = !write_messages_.empty();
-        this->write_messages_.push_back(msg);
-
-        if (!write_in_progress) {
-            boost::asio::async_write(socket_,
-                    boost::asio::buffer(write_messages_.front(),
-                    write_messages_.front().length()),
-                    boost::bind(&IRCClient::handle_write, this,
-                    boost::asio::placeholders::error));
-        }
-    }
-
-    void handle_write(const boost::system::error_code& error) {
-        if (!error) {
-            write_messages_.pop_front();
-            if (!write_messages_.empty()) {
-                boost::asio::async_write(socket_,
-                        boost::asio::buffer(write_messages_.front(),
-                        write_messages_.front().length()),
-                        boost::bind(&IRCClient::handle_write, this,
-                        boost::asio::placeholders::error));
-            }
-        } else {
-            do_close();
-        }
-    }
-
-    void do_close() {
-        socket_.close();
-    }
-
 private:
     boost::asio::io_service& io_service_;
     tcp::socket socket_;
     IRCMessage _read_msg;
-
-    irc_message_queue write_messages_;
     boost::asio::streambuf _response;
+    irc_message_queue write_messages_;
     
-    void process_message(IRCMessage msg) {
-        std::cout << msg.get_message() << std::endl;
-        std::cout << msg.get_command() << std::endl;
-    }
+    void handle_connect(const boost::system::error_code& error,
+            tcp::resolver::iterator endpoint_iterator);
+    void handle_read(const boost::system::error_code& error);
+    void do_write(std::string msg);
+    void handle_write(const boost::system::error_code& error);
+    void do_close();
+    void process_message(IRCMessage msg);
 };
+
 
 
 #endif	/* IRCCLIENT_HPP */
